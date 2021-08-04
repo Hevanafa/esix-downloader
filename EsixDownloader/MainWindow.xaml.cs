50819762e0b211b79166ea69bac2e5f188c9177d
Hevanafa <dhevansafa4@gmail.com>
2021-08-04 21:02:12 +0700

    Fixed frozen WebClient bug when DL'ing 2nd file

--- EsixDownloader/MainWindow.xaml.cs
+++ EsixDownloader/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Net;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Windows;
 
 namespace EsixDownloader
@@ -23,8 +24,9 @@ namespace EsixDownloader
 
         const string ChromeUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36";
 
-        async void startDownload() {
-            WebClient wc = null;
+        async void startDownload()
+        {
+            prgDownload.Value = 0;
 
             try
             {
@@ -104,7 +106,7 @@ namespace EsixDownloader
                 var targetFilename = Path.Combine(
                     "Downloads",
                     saveFilename + ext
-                    //Path.GetFileName(href)
+                //Path.GetFileName(href)
                 );
 
                 txbOutput.Text += "Starting download...\n";
@@ -115,16 +117,51 @@ namespace EsixDownloader
                 txbOutput.Text += $"File size: {res.ContentLength} bytes\n\n" +
                     $"Saving file as \"{targetFilename}\"\n";
 
-                wc = new WebClient();
-                wc.DownloadProgressChanged += Wc_DownloadProgressChanged;
-                wc.DownloadFileCompleted += Wc_DownloadFileCompleted;
-                await wc.DownloadFileTaskAsync(downloadLink, targetFilename);
+                req = null;
+                res.Dispose();
+
+                //txbOutput.Text += "Starting a new download thread\n";
+
+                //Thread.CurrentThread.IsBackground = true;
+
+                // https://stackoverflow.com/questions/1585985/how-to-use-the-webclient-downloaddataasync-method-in-this-context
+                using (var wc = new WebClient())
+                {
+                    wc.DownloadProgressChanged += Wc_DownloadProgressChanged;
+
+                    await wc.DownloadFileTaskAsync(downloadLink, "temp_download");
+                    //wc.DownloadDataCompleted += (sender, e) =>
+                    //{
+                    //};
+
+                    //txbOutput.Text += "Writing to file...\n";
+
+                    //var fileData = e.Result;
+
+                    // var fs = new FileStream("temp_download", FileMode.Create);
+                    // await fs.WriteAsync(fileData, 0, fileData.Length);
+                    // fs.Close();
+                    // fs.Dispose();
+
+                    // wc.DownloadData(downloadLink);
+                }
+
+                if (File.Exists(targetFilename))
+                    File.Delete(targetFilename);
+
+                File.Move("temp_download", targetFilename);
+
+                txbOutput.Text += "Download is completed.\n";
+
+                //wc.Dispose();
+                //}).Start();
             }
             catch (Exception ex)
             {
                 txbOutput.Text += "Error: " + ex.Message + "\n";
             }
-            finally {
+            finally
+            {
                 btnDownload.IsEnabled = true;
             }
         }
