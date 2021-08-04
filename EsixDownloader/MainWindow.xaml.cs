a6e29dd4a62a3c1dee73466462553b3ce1f7e04b
Hevanafa <dhevansafa4@gmail.com>
2021-08-04 19:25:44 +0700

    First upload

---
+++ EsixDownloader/MainWindow.xaml.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace EsixDownloader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (!Directory.Exists("Downloads"))
+                Directory.CreateDirectory("Downloads");
+        }
+
+        const string ChromeUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36";
+
+        async void startDownload() {
+            WebClient wc = null;
+
+            try
+            {
+                var uri = new Uri(txbUri.Text);
+
+                if (!uri.Host.Contains("e621.net"))
+                    throw new ArgumentException("The inputted URL is not part of e621.net.");
+
+                if (!txbUri.Text.Contains("e621.net/posts"))
+                    throw new ArgumentException("The inputted URL should be a link to an e621 post.");
+
+                txbOutput.Text += "Starting download...\n";
+
+                var req = (HttpWebRequest)WebRequest.Create(uri);
+                req.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+                req.UserAgent = ChromeUserAgent;
+
+                var res = await req.GetResponseAsync();
+                var sr = new StreamReader(res.GetResponseStream());
+                var HtmlContent = await sr.ReadToEndAsync();
+
+                // Done: get artists' names
+                var artistTagList = Regex.Match(
+                    HtmlContent,
+                    @"(?<=<ul class=""artist-tag-list"">)(.*?)(?=</ul>)",
+                    RegexOptions.Singleline
+                ).Value;
+
+                var anchors = Regex.Matches(
+                    artistTagList,
+                    "<a(.*?)>(.*?)</a>",
+                    RegexOptions.Singleline
+                ).Cast<Match>()
+                    .Select(x => x + "")
+                    .ToArray();
+
+                var artists = anchors
+                    .Where(x => x.Contains("itemprop=\"author\""))
+                    .Select(anchorTag =>
+                        Regex.Match(
+                            anchorTag,
+                            "(?<=<a(.*?)>)(.*?)(?=</a>)"
+                        ).Value
+                    ).ToArray();
+
+                var saveFilename = string.Join(",", artists)
+                    + " - " +
+                    Path.GetFileName(uri.GetLeftPart(UriPartial.Path));
+
+                //Debug.Print(saveFilename);
+
+                // Dispose everything
+                req = null;
+                res.Dispose();
+                sr.Dispose();
+
+                txbOutput.Text += $"The page has been fully received ({HtmlContent.Length} chars).\n";
+
+                //image-download-link
+                var anchor = Regex.Match(
+                    HtmlContent,
+                    @"(?<=<div id=""image-download-link"">)(.*?)(?=<\/div>)",
+                    RegexOptions.Singleline
+                ).Value;
+
+                var href = Regex.Match(
+                    anchor,
+                    @"(?<=href="")(.*?)(?="")",
+                    RegexOptions.Singleline
+                ).Value;
+
+                txbOutput.Text += $"Found the download link:\n{href}\n";
+
+                var downloadLink = new Uri(href);
+                var ext = Path.GetExtension(href);
+
+                var targetFilename = Path.Combine(
+                    "Downloads",
+                    saveFilename + ext
+                    //Path.GetFileName(href)
+                );
+
+                txbOutput.Text += "Starting download...\n";
+
+                req = (HttpWebRequest)WebRequest.Create(downloadLink);
+                res = await req.GetResponseAsync();
+
+                txbOutput.Text += $"File size: {res.ContentLength} bytes\n\n" +
+                    $"Saving file as \"{targetFilename}\"\n";
+
+                wc = new WebClient();
+                wc.DownloadProgressChanged += Wc_DownloadProgressChanged;
+                wc.DownloadFileCompleted += Wc_DownloadFileCompleted;
+                await wc.DownloadFileTaskAsync(downloadLink, targetFilename);
+            }
+            catch (Exception ex)
+            {
+                txbOutput.Text += "Error: " + ex.Message + "\n";
+            }
+            finally {
+                btnDownload.IsEnabled = true;
+            }
+        }
+
+        private void Wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            prgDownload.Value = e.BytesReceived / (double)e.TotalBytesToReceive * 100d;
+        }
+
+        private void Wc_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            prgDownload.Value = 100d;
+            txbOutput.Text += "Download is completed.\n";
+        }
+
+        private void BtnDownload_Click(object sender, RoutedEventArgs e)
+        {
+            btnDownload.IsEnabled = false;
+            startDownload();
+        }
+
+        private void BtnDownloadsFolder_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(
+                "explorer.exe",
+
+                Path.Combine(
+                    AppDomain.CurrentDomain.BaseDirectory,
+                    "Downloads"
+                )
+            );
+        }
+
+        private void TxbOutput_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            txbOutput.ScrollToEnd();
+        }
+    }
+}
